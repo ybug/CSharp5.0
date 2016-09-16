43d5b3fbf1c36aa19f91a110a8967731c1bbf875
ybug <ybug@naver.com>
2016-09-16 13:11:55 +0900

    OK

---
+++ HelloWorld/HelloWorld/HelloWorld.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BrainCSharp
+{
+    class HelloWorld
+    {
+        // 프로그램 실행이 시작되는 곳
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!"); // Hello, World를 프롬프트에 출력
+            //Console.WriteLine("Hello, {0}!", args[0]);
+            Exam1();
+        }
+
+        static void Exam1()
+        {
+            Console.WriteLine("여러분, 안녕하세요?");
+            Console.WriteLine("반갑습니다!");
+        }
+    }
+}
