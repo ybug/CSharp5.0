56f935c422f62fd0dfbc73115cff845bf6e3911c
ybug <ybug@naver.com>
2016-09-26 17:44:18 +0900

    Ok

---
+++ 07/ExtensionMethod/MainApp.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using MyExtension;
+
+namespace MyExtension
+{
+    public static class IntegerExtension
+    {
+        public static int Square(this int myInt)
+        {
+            return myInt * myInt;
+        }
+
+        public static int Power(this int myInt, int exponent)
+        {
+            int result = myInt;
+
+            for (int i = 1; i < exponent; i++)
+            {
+                result = result * myInt;
+            }
+
+            return result;
+        }
+    }
+
+    public static class StringExtension
+    {
+        public static string Append(this string myString, string myAppend)
+        {
+            return myString + myAppend;
+        }
+    }
+}
+
+namespace ExtensionMethod
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("{0}^2 : {1}", 3, 3.Square());
+            Console.WriteLine("{0}^{1} : {2}", 3, 4, 3.Power(4));
+            Console.WriteLine("{0}^{1} : {2}", 2, 10, 2.Power(10));
+
+            string hello = "hello";
+            Console.WriteLine(hello.Append(", World!"));
+        }
+    }
+}
