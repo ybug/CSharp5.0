2eaa33bd6063f8fd87a8b7701a5dd632a2264e5f
ybug <ybug@naver.com>
2016-09-26 17:14:18 +0900

    OK

---
+++ 04/Charpter04/Exam05/MainApp.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace Exam05
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            string num = Console.ReadLine();
+
+            switch (num)
+            {
+                case "1":
+                    {
+                        Exam01();
+                    }
+                    break;
+                case "2":
+                    {
+                        Exam02();
+                    }
+                    break;
+                case "3":
+                    {
+                        Exam03();
+                    }
+                    break;
+                case "4":
+                    {
+                    }
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        private static void Exam03()
+        {
+            int i = 0;
+
+            while (i < 5)
+            {
+                int j = 0;
+
+                while (true)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine("");
+            }
+            
+            //throw new NotImplementedException();
+        }
+
+        private static void Exam02()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5 - i; j++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine("");
+
+            }
+
+            //throw new NotImplementedException();
+        }
+
+        private static void Exam01()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < i + 1; j++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine("");
+            }
+
+            //throw new NotImplementedException();
+        }
+    }
+}
---
+++ 07/SealedMethod/MainApp.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace SealedMethod
+{
+    class Base
+    {
+        public virtual void SealMe()
+        {
+
+        }
+    }
+
+    class Derived : Base
+    {
+        public override void SealMe()
+        {
+
+        }
+    }
+
+    class WantToOverride : Derived
+    {
+        public override void SealMe()
+        {
+
+        }
+    }
+
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
