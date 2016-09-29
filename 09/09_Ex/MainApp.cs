f4204669ae61c5015b88a619c67159583a72cdd2
ybug <ybug@naver.com>
2016-09-29 19:08:52 +0900

    OK

---
+++ 09/09_Ex/MainApp.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace _09_Ex
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            int[,] a = new int[,] { { 3, 2 }, { 1, 4 } };
+            int[,] b = new int[,] { { 9, 2 }, { 1, 7 } };
+
+            Print(a, "A");
+            Console.WriteLine();
+            Print(name: "B", array: b);
+            Console.WriteLine();
+
+            int[,] c = Calc(a, b);
+            Print(c, "C");
+        }
+
+        private static int[,] Calc(int[,] a, int[,] b)
+        {
+            int[,] c = new int[2, 2];
+
+            c[0, 0] = a[0, 0] * b[0, 0];
+            c[0, 1] = a[0, 1] * b[1, 0];
+            c[1, 0] = a[1, 0] * b[0, 1];
+            c[1, 1] = a[1, 1] * b[1, 1];
+
+            return c;
+        }
+
+        private static void Print(int[,] array, string name)
+        {
+            Console.WriteLine("{0} = ", name);
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write("{0} ", array[i,j]);
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ 09/2DArray/MainApp.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections;
+
+namespace _2DArray
+{
+    class MyList/* : IEnumerable, IEnumerator*/
+    {
+        private int[] array;
+
+        public MyList()
+        {
+            array = new int[3];
+        }
+
+        public int this[int index]
+        {
+            get
+            {
+                return array[index];
+            }
+
+            set
+            {
+                if (index >= array.Length)
+                {
+                    Array.Resize<int>(ref array, index + 1);
+                    Console.WriteLine("Array Resized : {0}", array.Length);
+                }
+
+                array[index] = value;
+            }
+        }
+
+        public int Length
+        {
+            get
+            {
+                return array.Length;
+            }
+        }
+    }
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            //int[,] arr = new int[2, 3] { { 1, 2, 3 }, { 4, 5, 6 } };
+
+            //foreach (var items in arr)
+            //{
+            //    Console.WriteLine(items);
+            //}
+
+            //JaggedArray();
+            Indexer();
+        }
+
+        private static void Indexer()
+        {
+            MyList list = new MyList();
+            for (int i = 0; i < 5; i++)
+            {
+                list[i] = i;
+            }
+
+            for (int i = 0; i < list.Length; i++)
+            {
+                Console.WriteLine(list[i]);
+            }
+        }
+
+        static void JaggedArray()
+        {
+            int[][] jagged = new int[3][];
+            jagged[0] = new int[5] { 1, 2, 3, 4, 5 };
+            jagged[1] = new int[] { 10, 20, 30 };
+            jagged[2] = new int[] { 100, 200 };
+
+            foreach (int[] arr in jagged)
+            {
+                Console.Write("Length : {0}, ", arr.Length);
+
+                foreach (int e in arr)
+                {
+                    Console.Write("{0} ", e);
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
